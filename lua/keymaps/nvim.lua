1cd4b0874e682173de863ec3844143236699f2aa
Kyer Potts <kyerpotts@gmail.com>
2024-03-10 20:08:46 +0800

    Added save keymaps and created lsp file

--- lua/keymaps/nvim.lua
+++ lua/keymaps/nvim.lua
@@ -6,6 +6,7 @@ return {
   vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" }),
   vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" }),
   vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" }),
-  vim.keymap.set("n", "<leader>q", "<cmd>wq<CR>", { desc = "Quit" })
+  vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit" }),
+  vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save" }),
 }
 
