a856cb55dc8268937ddc19c5dbd1d2f694ad759f
Kyer Potts <kyerpotts@gmail.com>
2024-03-17 18:30:31 +0800

    Updated language tools with none-ls

--- lua/plugins/languagetools.lua
+++ lua/plugins/languagetools.lua
@@ -1,68 +1,73 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "williamboman/mason-lspconfig.nvim",
-      --"WhoIsSethDaniel/mason-tool-installer.nvim.nvim",
-      { "folke/neodev.nvim", opts = {} },
-      { "j-hui/fidget.nvim", opts = {} },
-    },
-    config = function()
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("milky-lsp-attach", { clear = true }),
-        callback = function(event)
-          local map = function(mode, keys, func, opts)
-            opts = vim.tbl_extend("force", opts, { buffer = event.buf })
-            vim.keymap.set(mode, keys, func, opts)
-          end
-          local lspkeymaps = require("keymaps.lsp")
-          local bindings = lspkeymaps.get_bindings()
-          for _, bind in ipairs(bindings) do
-            local mode, keys, func, opts = unpack(bind)
-            map(mode, keys, func, opts)
-          end
-        end,
-      })
-      require("mason").setup({})
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "jdtls", "lemminx", "pyright", "ruff_lsp" },
-      })
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      -- lspconfig.jdtls.setup({})
-      -- lspconfig.lemminx.setup({})
-      -- lspconfig.pyright.setup({})
-    end,
-  },
-  {
-    "linux-cultist/venv-selector.nvim",
-    opts = {},
-    keys = {
-      { "<leader>lv", "<cmd>VenvSelect<cr>", { desc = "Select Python [V]irtual En[V]ironment" } }
-    },
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "nvimtools/none-ls.nvim",
-    },
-    config = function()
-      -- require("mason").setup()
-      require("mason-null-ls").setup({
-        ensure_installed = {
-          "stylua",
-        },
-        automatic_installation = false,
-        handlers = {},
-      })
-      require("null-ls").setup({
-        sources = {
-          require("null-ls").builtins.formatting.stylua,
-        }
-      })
-    end,
-  }
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			--"WhoIsSethDaniel/mason-tool-installer.nvim.nvim",
+			{ "folke/neodev.nvim", opts = {} },
+			{ "j-hui/fidget.nvim", opts = {} },
+		},
+		config = function()
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("milky-lsp-attach", { clear = true }),
+				callback = function(event)
+					local map = function(mode, keys, func, opts)
+						opts = vim.tbl_extend("force", opts, { buffer = event.buf })
+						vim.keymap.set(mode, keys, func, opts)
+					end
+					local lspkeymaps = require("keymaps.lsp")
+					local bindings = lspkeymaps.get_bindings()
+					for _, bind in ipairs(bindings) do
+						local mode, keys, func, opts = unpack(bind)
+						map(mode, keys, func, opts)
+					end
+				end,
+			})
+			-- TODO: need to set up completion engines with snippets
+			require("mason").setup({})
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "jdtls", "lemminx", "pyright", "ruff_lsp" },
+			})
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			-- lspconfig.jdtls.setup({})
+			-- lspconfig.lemminx.setup({})
+			lspconfig.pyright.setup({})
+		end,
+	},
+	{
+		"linux-cultist/venv-selector.nvim",
+		opts = {},
+		keys = {
+			{ "<leader>lv", "<cmd>VenvSelect<cr>", desc = "Select Python [V]irtual En[V]ironment" },
+		},
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			-- require("mason").setup()
+			require("mason-null-ls").setup({
+				ensure_installed = {
+					"stylua",
+					"black",
+					"isort",
+				},
+				automatic_installation = false,
+				handlers = {},
+			})
+			require("null-ls").setup({
+				sources = {
+					require("null-ls").builtins.formatting.stylua,
+					require("null-ls").builtins.formatting.black,
+					require("null-ls").builtins.formatting.isort,
+				},
+			})
+		end,
+	},
 }
