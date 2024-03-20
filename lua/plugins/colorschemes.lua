24ad9709d2e7e796954564dd398d090723e6d605
Kyer Potts <kyerpotts@gmail.com>
2024-03-20 10:57:04 +0800

    Fixed colorscheme to show brackets and parantheses properly, as well as making the whitespace color less obtrusive

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,39 +1,52 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        styles = {
-          commends = { "italic" },
-          conditionals = { "italic" },
-        },
-        integrations = {
-          cmp = true,
-          nvimtree = true,
-          treesitter = true,
-          notify = true,
-          mini = {
-            enabled = true,
-            indentscope_color = ""
-          },
-        },
-      })
-    end,
-  },
-  {
-    "decaycs/decay.nvim",
-    name = "decay",
-    priority = 1000,
-    config = function()
-      require("decay").setup({
-        style = "decayce",
-        italics = {
-          comments = true,
-        },
-      })
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				styles = {
+					commends = { "italic" },
+					conditionals = { "italic" },
+				},
+				integrations = {
+					cmp = true,
+					nvimtree = true,
+					treesitter = true,
+					notify = true,
+					mini = {
+						enabled = true,
+						indentscope_color = "",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"decaycs/decay.nvim",
+		name = "decay",
+		priority = 1000,
+		config = function()
+			local style = "decayce"
+			local colors = require("decay.core").get_colors(style)
+			require("decay").setup({
+				style = style,
+				italics = {
+					code = false,
+					comments = true,
+				},
+				cmp = {
+					block_kind = true,
+				},
+				nvim_tree = {
+					contrast = true,
+				},
+				override = {
+					["@punctuation.bracket"] = { fg = colors.red },
+          Whitespace = { fg = colors.black }
+				},
+			})
+		end,
+	},
 }
