1353875e4f76e357b1d0c196b27e809834d00fb3
Kyer Potts <kyerpotts@gmail.com>
2024-03-20 10:54:25 +0800

    Removed unused autocmd for changing color of brackets

--- lua/autocommands/init.lua
+++ lua/autocommands/init.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "highlight when yanking text",
-  group = vim.api.nvim_create_augroup("milky-highlight-yank", {clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "highlight when yanking text",
+	group = vim.api.nvim_create_augroup("milky-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
