7599fa730f9578f4aebcea980d5a170a775e22d9
Kyer Potts <kyerpotts@gmail.com>
2024-03-22 22:34:02 +0800

    Updated harpoon keybindings

--- lua/keymaps/harpoon.lua
+++ lua/keymaps/harpoon.lua
@@ -3,10 +3,10 @@ local harpoon = require("harpoon")
 vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>hh", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
