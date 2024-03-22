d312e38098303060c5ce99100ace51351b4beaec
Kyer Potts <kyerpotts@gmail.com>
2024-03-22 23:11:24 +0800

    Fixed indentscope and removed noice

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,9 +1,9 @@
 return {
 	"echasnovski/mini.nvim",
-  config = function ()
-    require("mini.ai").setup({ n_lines = 500 })
-    require("mini.surround").setup()
-    require("mini.animate").setup()
-    require("mini.indentscope").setup()
-  end
+	config = function()
+		require("mini.ai").setup({ n_lines = 500 })
+		require("mini.surround").setup()
+		require("mini.animate").setup()
+		require("mini.indentscope").setup()
+	end,
 }
