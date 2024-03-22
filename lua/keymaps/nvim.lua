0c4450fad127abd6480d8c620334b80af3fce13a
Kyer Potts <kyerpotts@gmail.com>
2024-03-22 23:10:10 +0800

    Added keybinds for moving text up and down

--- lua/keymaps/nvim.lua
+++ lua/keymaps/nvim.lua
@@ -12,4 +12,10 @@ return {
   vim.keymap.set("n", "<leader>L", "<cmd>Lazy<CR>", { desc = "[L]azy" }),
   vim.keymap.set("n", "<leader>a", "<cmd>Alpha<CR>", { desc = "[A]lpha" }),
   vim.keymap.set("n", "<leader>bc", "<cmd>bnext<CR><cmd>bd #<CR>", { desc = "[B]uffer [C]lose" }),
+  vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move down" }),
+  vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move up" }),
+  vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" }),
+  vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" }),
+  vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move down" }),
+  vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" }),
 }
