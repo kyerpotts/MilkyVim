1c19936771341408cec0b7595cd32429d3e647f6
Kyer Potts <kyerpotts@gmail.com>
2024-04-28 11:13:25 +0800

    Moved autotag plugin to autopairs file and deleted autotag.lua

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,7 +1,20 @@
 return {
-  "altermo/ultimate-autopair.nvim",
-  event = { "InsertEnter", "CmdlineEnter" },
-  branch = "v0.6",
-  opts = {
-  },
+	{
+		"altermo/ultimate-autopair.nvim",
+		event = { "InsertEnter", "CmdlineEnter" },
+		branch = "v0.6",
+		opts = {},
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		ft = {
+			"javascript",
+			"javascriptreact",
+			"typescript",
+			"typescriptreact",
+		},
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	},
 }
