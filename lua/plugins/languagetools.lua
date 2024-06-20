0607cdc736550cde42f226f4af54876096b23bac
Kyer Potts <kyerpotts@gmail.com>
2024-06-20 23:05:44 +0800

    Removed lsp formatters from list, conform will fall back to lsp regardless

--- lua/plugins/languagetools.lua
+++ lua/plugins/languagetools.lua
@@ -249,33 +249,31 @@ return {
       { "<leader>lv", "<cmd>VenvSelect<cr>", desc = "Select Python [V]irtual En[V]ironment" },
     },
   },
-  { -- Autoformat
+  { -- Autoformat TODO: Fix formatting so it won't use prettier
     "stevearc/conform.nvim",
-    lazy = false,
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
     keys = {
       {
         "<leader>lf",
         function()
-          require("conform").format({ async = true, lsp_fallback = true })
+          require("conform").format({ async = true, lsp_format = "fallback" })
         end,
         mode = "",
         desc = "[F]ormat buffer",
       },
     },
     opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
+      -- notify_on_error = true,
+      format_on_save = {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
+        timeout_ms = 500,
+        lsp_format = "fallback"
+      },
       formatters_by_ft = {
-        lua = { "stylua" },
+        -- lua = { "stylua" },
         -- Conform can also run multiple formatters sequentially
         python = { "isort", "black" },
         java = { "google_java_format" },
@@ -288,8 +286,8 @@ return {
         typescript = { "prettierd" },
         typescriptreact = { "prettierd" },
         json = { "prettierd" },
-        html = { "prettierd" },
-        css = { "prettierd" },
+        -- html = { "prettierd" },
+        -- css = { "prettierd" },
       },
     },
   },
