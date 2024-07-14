5da7363b8a5fe876f9f53282627643c2a5d7fc52
Kyer Potts <kyerpotts@gmail.com>
2024-07-14 21:45:46 +0800

    Changes to obsidian config

--- lua/keymaps/obsidian.lua
+++ lua/keymaps/obsidian.lua
@@ -1,18 +1,35 @@
 -- TODO: Have to fix all three functions to work properly
 return {
-  vim.keymap.set("n", "<leader>oi", function()
-    local file_path = "/home/squidmilk/Documents/Braincage/! Inbox/"
-        .. vim.fn.input("Enter a note name: ", "", "file")
-        .. ".md"
-    if vim.fn.empty(vim.fn.glob(file_path)) > 0 then
-      vim.cmd("silent! !mkdir -p " .. vim.fn.fnamemodify(file_path, ":h"))
-      vim.cmd("silent! !touch " .. file_path)
+  vim.keymap.set("n", "<leader>oi",
+    function()
+      -- local file_path = "/home/squidmilk/Documents/Braincage/! Inbox/"
+      local file_name = vim.fn.input("Enter a note name: ", "", "file") .. ".md"
+      local build_command = "ObsidianNew " .. file_name
+      vim.cmd(build_command)
+      -- if vim.fn.empty(vim.fn.glob(file_path)) > 0 then
+      --   vim.cmd("silent! !mkdir -p " .. vim.fn.fnamemodify(file_path, ":h"))
+      --   vim.cmd("silent! !touch " .. file_path)
+      -- end
+      -- vim.cmd("edit " .. file_path)
+      -- vim.cmd("write" .. file_path)
+      -- vim.cmd(":ObsidianTemplate Fleeting Notes Template")
     end
-    vim.cmd("edit " .. file_path)
-    vim.cmd("write" .. file_path)
-    vim.cmd(":ObsidianTemplate Fleeting Notes Template")
-  end, { desc = "Add [I]nbox Note" }),
-
+    , { desc = "Add [I]nbox Note" }),
+  vim.keymap.set("n", "<leader>ol",
+    function()
+      local file_path = "/home/squidmilk/Documents/Braincage/001\\ Literature\\ Notes/" ..
+          vim.fn.input("Enter a note name: ", "", "file") .. ".md"
+      if vim.fn.empty(vim.fn.glob(file_path)) > 0 then
+        vim.cmd("silent! !mkdir -p " .. vim.fn.fnamemodify(file_path, ":h"))
+        vim.cmd("silent! !touch " .. file_path)
+      end
+      vim.cmd("edit " .. file_path)
+      -- vim.cmd("write" .. file_path)
+      if vim.fn.empty(vim.fn.glob(file_path)) <= 0 then
+        vim.cmd("ObsidianTemplate Literature Notes Template")
+      end
+    end
+    , { desc = "Add [I]nbox Note" }),
   vim.keymap.set("n", "<leader>ot", function()
     local file_path = "/home/squidmilk/Documents/Braincage/003 Daily Notes/" .. os.date("%Y-%m-%d")
 
