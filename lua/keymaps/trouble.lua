4e7a986641ff4b5b7bac3097afda557bca838a66
Kyer Potts <kyerpotts@gmail.com>
2024-07-16 20:27:45 +0800

    Updated trouble quickfix toggle keymap

--- lua/keymaps/trouble.lua
+++ lua/keymaps/trouble.lua
@@ -1,17 +1,17 @@
 return {
-	vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "[X]Diagnostics" }),
-	-- vim.keymap.set("n", "<leader>xx", "<cmd>Trouble qflist toggle<cr>", { desc = "Quickfi[X] List" }),
-	-- vim.keymap.set("n", "<leader>xl", "<cmd>Trouble loclist toggle<cr>", { desc = "[L]ocation List" }),
-	vim.keymap.set(
-		"n",
-		"<leader>cl",
-		"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-		{ desc = "[L]SP Definitions / references / ... (Trouble)" }
-	),
-	vim.keymap.set(
-		"n",
-		"<leader>cs",
-		"<cmd>Trouble symbols toggle focus=false<cr>",
-		{ desc = "Symbols (Trouble)" }
-	),
+  vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "[X]Diagnostics" }),
+  vim.keymap.set("n", "<leader>xq", "<cmd>Trouble qflist toggle<cr>", { desc = "[Q]uickfix List" }),
+  -- vim.keymap.set("n", "<leader>xl", "<cmd>Trouble loclist toggle<cr>", { desc = "[L]ocation List" }),
+  vim.keymap.set(
+    "n",
+    "<leader>cl",
+    "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+    { desc = "[L]SP Definitions / references / ... (Trouble)" }
+  ),
+  vim.keymap.set(
+    "n",
+    "<leader>cs",
+    "<cmd>Trouble symbols toggle focus=false<cr>",
+    { desc = "Symbols (Trouble)" }
+  ),
 }
