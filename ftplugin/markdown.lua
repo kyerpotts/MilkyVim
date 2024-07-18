a089ac552fc720ef4919908122e3c46242365257
Kyer Potts <kyerpotts@gmail.com>
2024-07-18 15:34:14 +0800

    Made changes to markdown ftplugin to set tabs to 2 spaces

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -2,6 +2,15 @@ return {
   vim.api.nvim_create_autocmd("FileType", {
     pattern = "markdown",
     callback = function()
+      vim.opt.autoindent = true
+      vim.opt.smartindent = true
+      vim.opt.expandtab = true
+      vim.opt.tabstop = 2
+      vim.opt.softtabstop = 2
+      vim.opt.shiftwidth = 2
+      vim.opt.signcolumn = "yes"
+      vim.opt.hlsearch = true
+      vim.opt.cursorline = true
       vim.opt_local.wrap = true
       vim.opt_local.linebreak = true
       vim.opt_local.breakindent = true
